d924e73422ec2ec7ffaaebf85681b6e088ca3210
Iannaan <moshou5571228@gmail.com>
2024-01-24 02:35:30 +0800

    finish Banner version1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom';
 import { Carousel } from 'antd';
 
 import Banner1 from '../components/image/banner/banner1.png';
@@ -10,11 +10,15 @@ import Banner2 from '../components/image/banner/banner2.png';
 
 import Banner3 from '../components/image/banner/banner3.png';
 
+import Banner4 from '../components/image/banner/banner4.JPG';
+
+
 const BannerBox = styled.div`
     display: flex;
     padding-top: 30px;
     flex-direction: column;
     flex-wrap: wrap;
+    box-sizing: border-box;
     @media (min-width: 769px) {
         flex-direction: row;
         flex-wrap: nowrap;
@@ -34,9 +38,11 @@ const BannerContainer = styled.div`
     width: 100%;
     
 `
+
+
 const Image = styled.div`
     height: ${(props) => props.height}px;
-    /* width: 33%; */
+    width: 100%;
     background-image: ${(props) => `url(${props.url})`};
     background-position: center;
     background-size: cover;
@@ -58,7 +64,23 @@ const BannerSection = styled.div`
 `
 
 const BannerFeatureContainer = styled.div`
+    width: 100%; //要設定外層
+    padding: 1px;
+    display: flex;
+    background-color: white;
+    padding: 8px 0;
+    margin-bottom:24px;
+    border-bottom: 1px solid #aaa ;
+    border-left: 1px solid  #aaa;
+    border-right: 1px solid  #aaa;
 `
+
+const BannerFeatureColumn = styled.div`
+    width: 33%;  //才知道要展開三個
+    padding: 1px;
+    text-align: center;
+`
+
 
 const HomePage = () => {
     return (
@@ -67,22 +89,40 @@ const HomePage = () => {
            <BannerBox>
                 <BannerCaruselContainer>
                     <Carousel autoplay>
-                        <Image height={200} url={Banner1} />
-                        <Image height={200} url={Banner2} />
-                        <Image height={200} url={Banner3} />
+                        <Link to='/P001'>
+                             <Image height={200} url={Banner1} />
+                        </Link>
+                        <Link to='/P002'>
+                            <Image height={200} url={Banner4} />
+                        </Link>
+                        <Link to='/P003'>
+                             <Image height={200} url={Banner3} />
+                        </Link>
                     </Carousel>
                 </BannerCaruselContainer>
                 <BannerSection>
                     <BannerContainer>
-                         <Image height={100} url={Banner1} />
+                        <Link to='/P004'>
+                            <Image height={100} url={Banner3} />
+                        </Link>
                     </BannerContainer>
-                   <BannerContainer>
-                        <Image height={100} url={Banner3} />
-                   </BannerContainer>
-                 
+                    <BannerContainer>
+                        <Link to='/P005'>
+                             <Image height={100} url={Banner4} />
+                        </Link>     
+                    </BannerContainer>
+                   
                 </BannerSection>
+       
            </BannerBox>
-          
+           <BannerFeatureContainer>
+                        <BannerFeatureColumn> 十五天鑑賞期</BannerFeatureColumn>
+                        <BannerFeatureColumn> 退貨無負擔</BannerFeatureColumn>
+                        <BannerFeatureColumn> 假一賠二</BannerFeatureColumn>
+            </BannerFeatureContainer>
+            <Link to="/advertising">
+                <Image height={100} url={Banner2} />
+            </Link>
         </DefaultLayout>
     );
 }
