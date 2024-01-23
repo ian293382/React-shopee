207a9855e55cdfe39c829e1b755767407f1b7018
Iannaan <moshou5571228@gmail.com>
2024-01-24 02:03:47 +0800

    implement Carusel build banner top part

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,88 @@
 import React from 'react';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import styled from 'styled-components';
-import ClearFix from '../components/common/ClearFix';
 
+import { Carousel } from 'antd';
+
+import Banner1 from '../components/image/banner/banner1.png';
+
+import Banner2 from '../components/image/banner/banner2.png';
+
+import Banner3 from '../components/image/banner/banner3.png';
+
+const BannerBox = styled.div`
+    display: flex;
+    padding-top: 30px;
+    flex-direction: column;
+    flex-wrap: wrap;
+    @media (min-width: 769px) {
+        flex-direction: row;
+        flex-wrap: nowrap;
+    }
+
+`
+const BannerCaruselContainer = styled.div`
+    width: 100%;
+    padding: 4px;
+    @media (min-width: 769px) {
+        width: 66.67%;
+    }
+    
+`
+
+const BannerContainer = styled.div`
+    width: 100%;
+    
+`
+const Image = styled.div`
+    height: ${(props) => props.height}px;
+    /* width: 33%; */
+    background-image: ${(props) => `url(${props.url})`};
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+`;
+
+const BannerSection = styled.div`
+    width:100%;
+    padding: 4px;
+    box-sizing: border-box;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    @media (min-width: 769px) {
+            width: 33.3%;
+        }
+`
+
+const BannerFeatureContainer = styled.div`
+`
 
 const HomePage = () => {
     return (
         <DefaultLayout fixHeader>
-           <ClearFix />
-            Homepage
-            <p>lorem.Your partner for a prosperous financial journey
-                Leorm Finance is the vibrant initiative of a team of young professional entrepreneurs with a solid background in the financial sector. We have joined forces and expertise to create a platform that redefines the standards of fairness and efficiency in the financial world. Currently, we aim to be your trusted guide in navigating through complex financial operations. Our ambition does not stop here: we see ourselves becoming the market leader of non-banking financial institutions 
-                in Romania and a main player in the non-banking financial market within the European Union.
-            </p>
-            <p>lorem.Your partner for a prosperous financial journey
-                Leorm Finance is the vibrant initiative of a team of young professional entrepreneurs with a solid background in the financial sector. We have joined forces and expertise to create a platform that redefines the standards of fairness and efficiency in the financial world. Currently, we aim to be your trusted guide in navigating through complex financial operations. Our ambition does not stop here: we see ourselves becoming the market leader of non-banking financial institutions 
-                in Romania and a main player in the non-banking financial market within the European Union.
-            </p>
-            <p>lorem.Your partner for a prosperous financial journey
-                Leorm Finance is the vibrant initiative of a team of young professional entrepreneurs with a solid background in the financial sector. We have joined forces and expertise to create a platform that redefines the standards of fairness and efficiency in the financial world. Currently, we aim to be your trusted guide in navigating through complex financial operations. Our ambition does not stop here: we see ourselves becoming the market leader of non-banking financial institutions 
-                in Romania and a main player in the non-banking financial market within the European Union.
-            </p>
-            <p>lorem.Your partner for a prosperous financial journey
-                Leorm Finance is the vibrant initiative of a team of young professional entrepreneurs with a solid background in the financial sector. We have joined forces and expertise to create a platform that redefines the standards of fairness and efficiency in the financial world. Currently, we aim to be your trusted guide in navigating through complex financial operations. Our ambition does not stop here: we see ourselves becoming the market leader of non-banking financial institutions 
-                in Romania and a main player in the non-banking financial market within the European Union.
-            </p>
+           
+           <BannerBox>
+                <BannerCaruselContainer>
+                    <Carousel autoplay>
+                        <Image height={200} url={Banner1} />
+                        <Image height={200} url={Banner2} />
+                        <Image height={200} url={Banner3} />
+                    </Carousel>
+                </BannerCaruselContainer>
+                <BannerSection>
+                    <BannerContainer>
+                         <Image height={100} url={Banner1} />
+                    </BannerContainer>
+                   <BannerContainer>
+                        <Image height={100} url={Banner3} />
+                   </BannerContainer>
+                 
+                </BannerSection>
+           </BannerBox>
+          
         </DefaultLayout>
     );
 }
